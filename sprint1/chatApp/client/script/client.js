12882db22a2727d98ca6e6f6661c361d6a81e3d1
Sourav Anand <sauravanand324@gmail.com>
2024-02-29 01:39:33 +0530

    completed basic chat app

--- sprint1/chatApp/client/script/client.js
+++ sprint1/chatApp/client/script/client.js
@@ -2,6 +2,9 @@ const socket = io("http://localhost:8080/",{ transports : ["websocket"]});
 
 const title = document.getElementById("title");
 const peopleCount = document.getElementById("people-count");
+const main = document.getElementById("main");
+
+let user ;
 
 socket.on("welcome",(msg)=>{
     title.textContent = msg ;
@@ -10,3 +13,57 @@ socket.on("welcome",(msg)=>{
 socket.on("people-count",(count)=>{
     peopleCount.textContent = count ;
 });
+
+const enterRoom = document.getElementById("enter-room");
+
+enterRoom.addEventListener("click",()=>{
+    socket.emit("setUserName",document.getElementById("user-name").value);
+});
+
+socket.on("userExist",(data)=>{
+   main.innerHTML = " " ;
+   const message = document.createElement("h1");
+   message.textContent = data.description ;
+   main.append(message);
+})
+socket.on("setUser",(data)=>{
+   console.log(data);
+   user = data.userName ;
+   main.innerHTML = " " ;
+   const div = document.createElement("div");
+   const input = document.createElement("input");
+   input.setAttribute("id", "input-tag");
+   input.setAttribute("placeholder", "Enter your message");
+
+   const messageContainer = document.createElement("div");
+   messageContainer.setAttribute("id","message-container");
+  
+   const button = document.createElement("button");
+   button.textContent = "Send" ;
+
+   button.addEventListener("click",sendMessage);
+
+   div.append(input,button,messageContainer);
+   main.append(div);
+});
+
+socket.on("newMsg",(data)=>{
+    const messageContainer = document.getElementById("message-container");
+    const div = document.createElement("div");
+    const name = document.createElement("b");
+    const msg = document.createElement("i");
+
+    name.textContent = `${data.user} :` ;   
+    msg.textContent = `${data.message}`;
+
+    name.append(msg);
+    div.append(name);
+    messageContainer.appendChild(div);
+})
+
+function sendMessage(){
+    const msg = document.getElementById("input-tag").value ;
+    if(msg){
+        socket.emit("new-client-message",{"message":msg , "user":user});
+    }
+}
--- sprint1/chatApp/server/index.js
+++ sprint1/chatApp/server/index.js
@@ -14,20 +14,31 @@ app.get("/",(req,res)=>{
     res.send({"msg":"This is home route"});
 });
 
-httpServer.listen(8080);
+httpServer.listen(8080,()=>{
+    console.log(`Server is listening at http://localhost:${8080}`);
+});
 
-let count  = 0 ;
+let user = [] ;
 io.on("connection",(socket)=>{
     console.log("New client connected");
-    count++;
-    socket.emit("welcome","Welocome to Chat App");
-    io.emit("people-count",count);
+    socket.on("setUserName",(data)=>{
+        console.log(data);
+        if(user.indexOf(data) > -1){
+           socket.emit("userExist",{"description":`User already exits with ${data}`});
+        }else{
+            user.push(data);
+            socket.emit("setUser",{"userName" : data});
+        }
+    });
 
     socket.on("disconnect",()=>{
         console.log("New client disconnected");
-        count--;
-        socket.broadcast.emit("people-count",count);
     });
+
+    socket.on("new-client-message",(data)=>{
+        console.log(data);
+        io.sockets.emit("newMsg",data);
+    })
 });
 
 
